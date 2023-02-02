cb5a772743a9f228245cbdaeda085d98473011c7
Chase Harrison <chaseh88@gmail.com>
2023-02-01 23:01:16 -0600

    testing post with express and body parser

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Input } from "antd";
+import { Input, Button, Form as AntForm } from "antd";
+import { api } from "../utils/axios";
 
 const graphqlSchema = `
   type Query {
@@ -35,14 +36,40 @@ const { TextArea } = Input;
 const Form = () => {
   const [schema, setSchema] = useState(graphqlSchema);
 
+  const handleSubmit = async () => {
+    const { data } = await api.post("/post-schema", {
+      schema,
+    });
+    console.log(data);
+  };
+
   return (
-    <div>
-      <TextArea
-        rows={20}
-        value={schema}
-        onChange={(e) => setSchema(e.target.value)}
-      />
-    </div>
+    <AntForm
+      name="basic"
+      labelCol={{ span: 1 }}
+      wrapperCol={{ span: 16 }}
+      style={{ width: "100%" }}
+      autoComplete="off"
+      onFinish={handleSubmit}
+      initialValues={{ schema }}
+    >
+      <AntForm.Item
+        label="Schema"
+        name="schema"
+        rules={[{ required: true, message: "Please input your schema!" }]}
+      >
+        <TextArea
+          rows={4}
+          value={schema}
+          onChange={(e) => setSchema(e.target.value)}
+        />
+      </AntForm.Item>
+      <AntForm.Item wrapperCol={{ offset: 1, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </AntForm.Item>
+    </AntForm>
   );
 };
 
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
+import bodyParser from "body-parser";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
 import { App } from "../client/client";
 
 const server = express();
+server.use(bodyParser.json());
 const port = 3001;
 
 server.set("view engine", "ejs");
@@ -22,6 +24,11 @@ const assets = JSON.parse(manifest);
 server.get("/", (req, res) => {
   const component = ReactDOMServer.renderToString(React.createElement(App));
   res.render("client", { assets, component });
+});
+
+// create a route that accepts json
+server.post("/post-schema", (req, res) => {
+  console.log("req.body", req.body);
 });
 
 server.listen(port, () => {
